28b64b42b08737315ee345c8205738ec1bdb2d8b
kojima0725 <kojikoji0725kt@gmail.com>
2021-05-14 18:36:29 +0900

    スクリプトとかを作成した

---
+++ Assets/Script/Player/PlayerCar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// プレイヤーの乗っている車
+/// ゲームパッドの左側で操作する
+/// </summary>
+public class PlayerCar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
