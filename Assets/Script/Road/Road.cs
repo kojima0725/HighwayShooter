af589e5cb17e5b367d0647543c22f01cdfb22b1c
kojima0725 <kojikoji0725kt@gmail.com>
2021-05-18 14:15:18 +0900

    後ろに流れていく道を作成途中

---
+++ Assets/Script/Road/Road.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// 自身に所属しているRoadObjectを後ろに流していく
+/// </summary>
+public class Road : MonoBehaviour
+{
+    public static Road current;
+
+    private void Awake()
+    {
+        current = this;
+    }
+
+    /// <summary>
+    /// 速度
+    /// </summary>
+    [SerializeField]
+    float speed;
+
+    /// <summary>
+    /// 後ろに流していくオブジェクト達
+    /// </summary>
+    readonly List<Transform> roads = new List<Transform>();
+
+
+    /// <summary>
+    /// 後ろに流すオブジェクトを登録する
+    /// </summary>
+    /// <param name="obj"></param>
+    public void Join(Transform obj)
+    {
+        roads.Add(obj);
+    }
+
+
+    /// <summary>
+    /// 後ろに流すオブジェクトを解除する
+    /// </summary>
+    /// <param name="obj"></param>
+    public void Leave(Transform obj)
+    {
+        roads.Remove(obj);
+    }
+
+}
---
+++ Assets/Script/Road/RoadObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 後ろに流れるオブジェクト達
+/// </summary>
+public class RoadObject : MonoBehaviour
+{
+    /// <summary>
+    /// 自身が削除されるタイミング
+    /// </summary>
+    [SerializeField]
+    float deadPos;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Road.current.Join(transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z < deadPos)
+        {
+            Road.current.Leave(transform);
+            Destroy(this);
+        }
+    }
+}
