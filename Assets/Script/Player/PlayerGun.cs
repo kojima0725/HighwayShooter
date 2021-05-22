08f37553bdec9bd4afec2acc2be0a1db3e5b0ce3
kojima0725 <kojikoji0725kt@gmail.com>
2021-05-22 17:01:56 +0900

    銃のレティクルが動くように

--- Assets/Script/Player/PlayerGun.cs
+++ Assets/Script/Player/PlayerGun.cs
@@ -12,5 +12,38 @@ public class PlayerGun : MonoBehaviour
     /// レティクルの画像
     /// </summary>
     [SerializeField]
-    Image Reticle;
+    Image reticle;
+
+    [SerializeField]
+    float reticleSpeed;
+
+
+    [SerializeField]
+    bool debugMode;
+    [SerializeField]
+    Text debugText;
+
+    private void Update()
+    {
+        MoveReticle();
+        if (debugMode)
+        {
+            debugText.text = "X:" + reticle.rectTransform.position.x.ToString("000000") + "Y:" + reticle.rectTransform.position.y.ToString("000000");
+        }
+    }
+
+    /// <summary>
+    /// プレイヤーの入力に応じてレティクルを動かす
+    /// </summary>
+    private void MoveReticle()
+    {
+        Vector3 move = new Vector2(KInputManager.GetGunMoveInputX(), KInputManager.GetGunMoveInputY());
+        move.x *= reticleSpeed;
+        move.y *= reticleSpeed;
+        //上下反転の必要あり
+        move.y = -move.y;
+        move.z = 0;
+
+        reticle.rectTransform.position += move;
+    }
 }
