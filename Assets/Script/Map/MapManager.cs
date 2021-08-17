b1fcbfe4a2d848d25e9d4fb35fd26dc59ddb25e4
kojima0725 <kojikoji0725kt@gmail.com>
2021-08-17 22:58:59 +0900

    マップ生成

--- Assets/Script/Map/MapManager.cs
+++ Assets/Script/Map/MapManager.cs
@@ -2,7 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapManager : MonoBehaviour
+public class MapManager : MonoBehaviour,ICanGetTransforms
 {
-    
+    [SerializeField]
+    private World world;
+    [SerializeField]
+    private MapMaker mapMaker;
+
+    /// <summary>
+    /// マップチップをこのオブジェクト下に格納する
+    /// </summary>
+    [SerializeField]
+    private Transform mapChipContainer;
+
+    /// <summary>
+    /// 生成したマップチップ
+    /// </summary>
+    readonly List<MapChip> chips = new List<MapChip>();
+
+    public IEnumerable<Transform> Transforms()
+    {
+        foreach (var item in chips)
+        {
+            yield return item.transform;
+        }
+    }
 }
