f6cc9883f027ad078e3adc4072692a4e85121b59
kojima0725 <kojikoji0725kt@gmail.com>
2021-12-11 19:05:23 +0900

    やはり敵はついてきたほうが良い

---
+++ Assets/Script/NPC/Heli/HeliRoller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeliRoller : MonoBehaviour
+{
+    [SerializeField]
+    SnapAxis axis;
+    [SerializeField]
+    float speed;
+
+    private void Update()
+    {
+        float rotate = speed * Time.deltaTime;
+        if (axis == SnapAxis.X)
+        {
+            this.transform.Rotate(rotate, 0, 0);
+        }
+        if (axis == SnapAxis.Y)
+        {
+            this.transform.Rotate(0, rotate, 0);
+        }
+        if (axis == SnapAxis.Z)
+        {
+            this.transform.Rotate(0, 0, rotate);
+        }
+    }
+}
